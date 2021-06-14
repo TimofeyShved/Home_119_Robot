56f8cfd39647ec5eaecf625d522406474c7edcf2
timofeya123 <timofeya123@inbox.ru>
2021-06-15 00:44:41 +0600

    comment

--- src/com/AWT/Main.java
+++ src/com/AWT/Main.java
@@ -17,37 +17,37 @@ import java.util.Iterator;
 public class Main {
 
     public static void main(String[] args) {
-        JFrame jFrame = getFrame();
-        JPanel jPanel = new JPanel();
+        JFrame jFrame = getFrame(); // создание формы
+        JPanel jPanel = new JPanel(); // и панели
         jFrame.add(jPanel);
 
-        JButton button1 = new JButton("click 1");
-        button1.addActionListener(new ActionListener() {
+        JButton button1 = new JButton("click 1"); //  создание кнопки
+        button1.addActionListener(new ActionListener() { // действие
             @Override
             public void actionPerformed(ActionEvent e) {
-                jFrame.setTitle(jFrame.getTitle()+"+");
+                jFrame.setTitle(jFrame.getTitle()+"+"); // изминение титульника у формы
             }
         });
-        jPanel.add(button1);
+        jPanel.add(button1); // добавить кнопку на панель
 
-        JButton button2 = new JButton("click 2");
-        button2.addActionListener(new ActionListener() {
+        JButton button2 = new JButton("click 2"); // создание кнопки
+        button2.addActionListener(new ActionListener() { // действие
             @Override
             public void actionPerformed(ActionEvent e) {
-                GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
-                GraphicsDevice device = environment.getDefaultScreenDevice();
+                GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment(); // добаляем графику ПК
+                GraphicsDevice device = environment.getDefaultScreenDevice(); // узанём размеры экрана
                 try {
-                    Robot mrRobot = new Robot(device);
+                    Robot mrRobot = new Robot(device); // создаём робота, с полем действия
 
-                    mrRobot.mouseMove(500+225, 500+50);
-                    mrRobot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-                    mrRobot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                    mrRobot.mouseMove(500+225, 500+50); // перевести мышку
+                    mrRobot.mousePress(InputEvent.BUTTON1_DOWN_MASK); // нажать на кнопку мышки
+                    mrRobot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); // отпустить
 
-                    mrRobot.keyPress(KeyEvent.VK_TAB);
-                    mrRobot.keyRelease(KeyEvent.VK_TAB);
+                    mrRobot.keyPress(KeyEvent.VK_TAB); // нажать клавишу таб
+                    mrRobot.keyRelease(KeyEvent.VK_TAB); // отпустить
 
-                    Rectangle rectangle = new Rectangle(0,0,100,100);
-                    BufferedImage image = mrRobot.createScreenCapture(rectangle);
+                    Rectangle rectangle = new Rectangle(0,0,100,100); // размеры квадрата
+                    BufferedImage image = mrRobot.createScreenCapture(rectangle); // сделать скриншот
 
                     ImageIO.write(image, "png", new File("1.png")); // чтение файла и сохранение его в png
 
@@ -59,21 +59,22 @@ public class Main {
                     // запись картинки в папку
                     ImageOutputStream imageOutputStream = ImageIO.createImageOutputStream(new File("1.png"));
                     writer.setOutput(imageOutputStream);
+
                 } catch (AWTException | IOException ex) {
-                    ex.printStackTrace();
+                    ex.printStackTrace();// если есть ошибки
                 }
             }
         });
-        jPanel.add(button2);
-        jPanel.revalidate();
+        jPanel.add(button2); // добавить кнопку на панель
+        jPanel.revalidate(); // перерисовать
 
     }
 
-    static JFrame getFrame(){
+    static JFrame getFrame(){ // создание формы
         JFrame jFrame = new JFrame();
-        jFrame.setVisible(true);
-        jFrame.setBounds(500,500,500,200);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true); // видимость
+        jFrame.setBounds(500,500,500,200); // размеры
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // закрытие
         return jFrame;
     }
 }
